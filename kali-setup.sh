d74f4a561df6b325fbab92318a24cabb2c994a46
Aryan <aryanguenthner@users.noreply.github.com>
2018-03-27 15:15:44 -0700

    Add files via upload

--- kali-setup.sh
+++ kali-setup.sh
@@ -22,18 +22,22 @@ echo "Updating Kali"
 wget -q -O - https://archive.kali.org/archive-key.asc  | apt-key add
 apt update && apt -y upgrade && apt -y full-upgrade && apt clean && apt -y autoremove
 echo
-# Installing these make life easier
+# Installing these on Kali make life easier
 echo "Installing Tools"
 echo
 apt -y install build-essential nfs-common cifs-utils xutils-dev gimp vlc git gcc-6 curl openssl libreoffice git libmbim-utils terminator graphviz python-pygraphviz python-lxml python-libpcap python-qt4 python-pip python-imaging python3-dev xvfb cmake mongodb libappindicator1 libpcap-dev open-vm-tools-desktop fuse
 echo
 # Some of My Most Frequently Used Git Repositories
+echo
+# MiTM Network attack and monitoring framework.
 mkdir /opt/bettercap
 echo
 cd /opt/bettercap
 wget https://github.com/bettercap/bettercap/releases/download/v2.3.1/bettercap_linux_amd64_2.3.1.zip
 unzip bettercap_linux_amd64_2.3.1.zip
 echo
+sudo ln -s /usr/lib/x86_64-linux-gnu/libpcap.so.1.8.1 /usr/lib/libpcap.so.1
+# Enumerating data from Windows and Samba hosts
 cd /opt
 git clone https://github.com/portcullislabs/enum4linux.git
 echo
@@ -42,6 +46,7 @@ bunzip2 polenum-0.2.tar.bz2
 tar -xvf polenum-0.2.tar 
 cp polenum-0.2/polenum.py /usr/bin/
 echo
+# Bypassing Anti-Virus Maybe
 cd /opt
 git clone https://github.com/Veil-Framework/Veil.git
 cd Veil/setup
@@ -51,6 +56,7 @@ cd /opt
 echo
 git clone https://github.com/khr0x40sh/MacroShop.git
 echo
+# Impacket my fav
 cd /opt
 git clone https://github.com/CoreSecurity/impacket.git
 echo
@@ -58,14 +64,17 @@ cd impacket
 python setup.py install
 echo
 cd /opt
+# Web Resource Enumeration Tool HTTP Screenshot and Default Credential Checks
 git clone https://github.com/ChrisTruncer/EyeWitness.git
 cd EyeWitness/setup
 yes | ./setup.sh
 cd /opt
+# Web Resource Enumeration Tool
 git clone https://bitbucket.org/al14s/rawr.git
 cd rawr
 yes | ./install.sh
 echo
+# Remote Desktop Protocol
 cd /opt
 git clone https://github.com/citronneur/rdpy
 cd rdpy
@@ -73,10 +82,12 @@ python setup.py install
 cd /opt
 pip3 install gplaycli
 echo
+# A LinkedIn enumeration tool
 cd /opt
 git clone https://github.com/gojhonny/InSpy.git
 echo
 cd /opt
+# Install remote administration and post-exploitation tool
 git clone https://github.com/n1nj4sec/pupy.git
 cd pupy
 git submodule init
@@ -87,6 +98,13 @@ tar xvf payload_templates.txz && mv payload_templates/* pupy/payload_templates/ 
 echo
 cd /opt
 git clone https://github.com/leebaird/discover.git
+echo
+# Install Google Chrome Browser
+cd /tmp
+echo "Downloading and Installing Google Chrome Stable"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
+dpkg -i google-chrome-stable_current_amd64.deb
+sed -i '49s|.*|exec -a "$0" "$HERE/chrome" "$@" --no-sandbox --user-data-dir|' /usr/bin/google-chrome-stable
 echo
 # Enabling SSH
 echo "Enabling SSH"
